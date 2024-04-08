a4d50680f82ec7647cc98de1ca7c3b0828d6c08b
Артемий <artema121rus@mail.ru>
2024-04-09 00:40:05 +0300

    feat(task-3): создает классовый компонент

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Counter from "./components/Counter";
+import ClassCounter from "./components/ClassCounter";
 
 function App() {
   return (
     <div className="App">
       <Counter></Counter>
+      <ClassCounter></ClassCounter>
     </div>
   );
 }
---
+++ src/components/ClassCounter.jsx
@@ -0,0 +1,34 @@
+import { render } from "@testing-library/react";
+import React from "react";
+
+class ClassCounter extends React.Component {
+
+    constructor(props)  {
+        super(props);
+        this.state = {
+            count: 0
+        }
+        this.minus = this.minus.bind(this)
+        this.plus = this.plus.bind(this)
+    }
+
+    minus() {
+        this.setState({count: this.state.count - 1})
+    }
+    
+    plus() {
+        this.setState({count: this.state.count + 1})
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>{this.state.count}</h1>
+                <button onClick={this.minus}>minus</button>
+                <button onClick={this.plus}>plus</button>
+            </div>
+        )
+    }
+}
+
+export default ClassCounter;
