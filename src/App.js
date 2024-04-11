c1a8ab52033cb695a610470bca13ccb02c8cf53c
Артемий <artema121rus@mail.ru>
2024-04-11 21:20:32 +0300

    feat(task-10): добавляет функционал на кнопку удалить(пост)

--- src/App.js
+++ src/App.js
@@ -15,10 +15,15 @@ function App() {
     setPosts([...posts, newPost])
   }
 
+  // Получаем пост из дочернего элемента
+  const removePost = (post) => {
+    setPosts(posts.filter(p => p.id !== post.id))
+  }
+
   return (
     <div className="App">
       <PostForm create={createPost}></PostForm>
-      <PostList posts={posts} title="Спиосок постов 1"></PostList>
+      <PostList remove={removePost} posts={posts} title="Спиосок постов 1"></PostList>
     </div>
   );
 }
