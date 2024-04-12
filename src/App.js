5076e6f69857dc49790f6a558a7a5dc0f3b8da24
Артемий <artema121rus@mail.ru>
2024-04-12 13:23:04 +0300

    feat(task-11): добавляет механизм сортировки постов по названию и описанию

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import React, { useState } from "react";
 import './styles/App.css'
 import PostList from "./components/PostList";
 import PostForm from "./components/PostForm";
+import MySelect from "./components/UI/select/MySelect";
 
 function App() {
 
   const [posts, setPosts] = useState([
-    {id:1, title:'JS', body:'JS - язык програмирования'},
-    {id:2, title:'JS 2', body:'JS - язык програмирования'},
-    {id:3, title:'JS 3', body:'JS - язык програмирования'},
+    {id:1, title:'fff', body:'ggg'},
+    {id:2, title:'aaa', body:'vvv'},
+    {id:3, title:'ccc', body:'ddd'},
   ])
+
+  const [sortedSort, setSortedSort] = useState('')
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost])
@@ -19,11 +22,33 @@ function App() {
   const removePost = (post) => {
     setPosts(posts.filter(p => p.id !== post.id))
   }
+  
+  // После того, как пользователь выбрал алгоритм сортировки, нам нужно отсоритровать массив
+  // тут sort выбранный механизм сортировки - если вывести в лог, то получим title или body при выборе названия сортировки
+  const sortPosts = (sort) => {
+    // вызываем функцию setSortedSort, чтобы передать туда то, что приходит к нам из самого selecta - ту опцию, которую выбрали
+    setSortedSort(sort)
+    // вызываем функцию setPosts, чтобы передать отсортированный массив
+    setPosts([...posts].sort((a ,b) => a[sort].localeCompare(b[sort])))
+  }
 
   return (
     <div className="App">
       <PostForm create={createPost}></PostForm>
-      <PostList remove={removePost} posts={posts} title="Спиосок постов 1"></PostList>
+      <hr style={{margin: '15px 0'}}></hr>
+      <MySelect
+        value={sortedSort}
+        onChange={sortPosts}
+        defoultValue="Сортировка"
+        options={[
+          {value: 'title', name: 'По названию'},
+          {value: 'body', name: 'По описанию'}
+        ]}
+      ></MySelect>
+      {posts.length !== 0
+        ?  <PostList remove={removePost} posts={posts} title="Спиосок постов 1"></PostList>
+        :  <h1 style={{textAlign: 'center'}}>Посты не найдены!</h1> 
+      }
     </div>
   );
 }
---
+++ src/components/UI/select/MySelect.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const MySelect = ({ options, defoultValue, value, onChange}) => {
+    return (
+        <select
+        value={value}
+        // передаем не сам event, а значение, который выбрал пользователь-onChange(e.target.value)
+        onChange={e => onChange(e.target.value)}
+        >
+            <option disabled value=''>
+                {defoultValue}
+            </option>
+            {options.map((option) => (
+                <option key={option.value} value={option.value}>
+                    {option.name}
+                </option>
+            ))}
+        </select>
+    );
+};
+
+export default MySelect;
