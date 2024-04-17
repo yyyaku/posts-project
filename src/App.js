eda3c763ab5f6990a351a6572f47778c51850198
Артемий <artema121rus@mail.ru>
2024-04-17 15:44:52 +0300

    feat(task-12): добавляет механизм поиска постов по названию

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import React, { useState } from "react";
+import React, { useMemo, useState } from "react";
 import './styles/App.css'
 import PostList from "./components/PostList";
 import PostForm from "./components/PostForm";
 import MySelect from "./components/UI/select/MySelect";
+import MyInput from "./components/UI/input/MyInput";
 
 function App() {
 
@@ -12,32 +13,43 @@ function App() {
     {id:3, title:'ccc', body:'ddd'},
   ])
 
-  const [sortedSort, setSortedSort] = useState('')
+  const [selectedSort, setSelectedSort] = useState('')
+
+  const [searchQuery, setSearchQuery] = useState('')
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost])
   }
 
-  // Получаем пост из дочернего элемента
   const removePost = (post) => {
     setPosts(posts.filter(p => p.id !== post.id))
   }
-  
-  // После того, как пользователь выбрал алгоритм сортировки, нам нужно отсоритровать массив
-  // тут sort выбранный механизм сортировки - если вывести в лог, то получим title или body при выборе названия сортировки
+
+  const sortedPosts = useMemo(() => {
+    if(selectedSort) {
+      return [...posts].sort((a ,b) => a[selectedSort].localeCompare(b[selectedSort]))
+    } return posts;
+  }, [selectedSort, posts])
+
+  const sortedAndSearchedPosts = useMemo(() => {
+    return sortedPosts.filter(post => post.title.toLowerCase().includes(searchQuery))
+  }, [sortedPosts, searchQuery])
+
   const sortPosts = (sort) => {
-    // вызываем функцию setSortedSort, чтобы передать туда то, что приходит к нам из самого selecta - ту опцию, которую выбрали
-    setSortedSort(sort)
-    // вызываем функцию setPosts, чтобы передать отсортированный массив
-    setPosts([...posts].sort((a ,b) => a[sort].localeCompare(b[sort])))
+    setSelectedSort(sort)
   }
 
   return (
     <div className="App">
       <PostForm create={createPost}></PostForm>
       <hr style={{margin: '15px 0'}}></hr>
+      <MyInput
+        placeholder="Поиск..."
+        value={searchQuery}
+        onChange={e=>setSearchQuery(e.target.value)}
+      ></MyInput>
       <MySelect
-        value={sortedSort}
+        value={selectedSort}
         onChange={sortPosts}
         defoultValue="Сортировка"
         options={[
@@ -45,8 +57,8 @@ function App() {
           {value: 'body', name: 'По описанию'}
         ]}
       ></MySelect>
-      {posts.length !== 0
-        ?  <PostList remove={removePost} posts={posts} title="Спиосок постов 1"></PostList>
+      {sortedAndSearchedPosts.length !== 0
+        ?  <PostList remove={removePost} posts={sortedAndSearchedPosts} title="Спиосок постов!"></PostList>
         :  <h1 style={{textAlign: 'center'}}>Посты не найдены!</h1> 
       }
     </div>
