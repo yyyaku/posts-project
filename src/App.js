166dbab01db0942e042515cb5914c0a5f736ea19
Артемий <artema121rus@mail.ru>
2024-04-20 11:14:11 +0300

    feat(task-23): дорабатывает авторизацию

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./styles/App.css";
 import { BrowserRouter } from "react-router-dom";
 import Nav from "./components/UI/Nav/Nav";
@@ -7,12 +7,22 @@ import { AuthContext } from "./components/context";
 
 function App() {
     const [isAuth, setIsAuth] = useState(false);
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        if (localStorage.getItem("Auth")) {
+            setIsAuth(true);
+            setIsLoading(false);
+        }
+    }, []);
+
     return (
         <div>
             <AuthContext.Provider
                 value={{
                     isAuth,
                     setIsAuth,
+                    isLoading,
                 }}
             >
                 <BrowserRouter>
